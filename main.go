95625a9ee1eb3d3e029549d9c56083826ac71b3a
Sourjaya <sourjaya2013@gmail.com>
2024-05-27 20:40:44 +0530

    added test client

--- main.go
+++ main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"sync"
 
 	"github.com/gliderlabs/ssh"
 	"github.com/joho/godotenv"
+	"github.com/teris-io/shortid"
 	gossh "golang.org/x/crypto/ssh"
 )
 
-func main() {
+func startSSHServer() error {
+	// respCh := make(chan string)
+	// go func() {
+	// 	time.Sleep(time.Second * 3)
+	// 	id, _ := shortid.Generate()
+	// 	respCh <- "http://webhooker.com/" + id
+
+	// 	time.Sleep(time.Second * 10)
+	// 	for {
+	// 		time.Sleep(time.Second * 2)
+	// 		respCh <- "received data from hook"
+	// 	}
+	// }()
+	handler := NewSSHHandler()
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Failed to load environment variables")
@@ -24,7 +42,7 @@ func main() {
 
 	server := ssh.Server{
 		Addr:    ":" + sshPort,
-		Handler: handleSSHSession,
+		Handler: handler.handleSSHSession,
 		ServerConfigCallback: func(ctx ssh.Context) *gossh.ServerConfig {
 			cfg := &gossh.ServerConfig{
 				ServerVersion: "SSH-2.0-sendit",
@@ -46,10 +64,96 @@ func main() {
 	}
 	server.AddHostKey(privateKey)
 	fmt.Println("Server running at port: ", sshPort)
-	log.Fatal(server.ListenAndServe())
+	return server.ListenAndServe()
 }
 
-func handleSSHSession(session ssh.Session) {
-	session.R
-	fmt.Println("Hello")
+var clients sync.Map
+
+type HTTPHandler struct {
+}
+
+func (h *HTTPHandler) handleWebhook(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	ch, ok := clients.Load(id)
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("client id not found"))
+		return
+	}
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+
+	ch.(chan string) <- string(b)
+}
+
+func startHTTPServer() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load environment variables")
+	}
+
+	httpPort := os.Getenv("HTTP_PORT")
+
+	if httpPort == "" {
+		log.Fatal("HTTP PORT not found in the environment")
+	}
+	router := http.NewServeMux()
+
+	handler := &HTTPHandler{}
+	router.HandleFunc("/{id}/*", handler.handleWebhook)
+	fmt.Println("HTTP Server running at port: ", httpPort)
+	return http.ListenAndServe(":"+httpPort, router)
+}
+
+func main() {
+	go startSSHServer()
+	startHTTPServer()
+}
+
+type SSHHandler struct {
+	channels map[string]chan string
+}
+
+func NewSSHHandler() *SSHHandler {
+	return &SSHHandler{
+		channels: make(map[string]chan string),
+	}
+}
+
+func (h *SSHHandler) handleSSHSession(session ssh.Session) {
+	// forwardURL := session.RawCommand()
+	// _ = forwardURL
+	// webhookURL := <-h.respCh
+	cmd := session.RawCommand()
+	if cmd == "init" {
+		id := shortid.MustGenerate()
+		webhookURL := "http://localhost:5000/" + id + "\n"
+		resp := fmt.Sprintf("webhook url %sssh localhost -p 2222 %s | curl -X POST -H 'Content-Type: application/json' -d @- http://localhost:3000/payment/webhook\n", webhookURL, id)
+		session.Write([]byte(resp))
+		respCh := make(chan string, 1)
+		h.channels[id] = respCh
+		// respCh := make(chan string)
+		clients.Store(id, respCh)
+	}
+	if len(cmd) > 0 && cmd != "init" {
+		respCh, ok := h.channels[cmd]
+		if !ok {
+			session.Write([]byte("invalid webhook id\n"))
+			return
+		}
+		for data := range respCh {
+			session.Write([]byte(data + "\n"))
+		}
+	}
+	// for {
+	// 	n, err := session.Read(buf)
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// 	log.Fatal(err)
+	// 	fmt.Println(string(buf[:n]))
+	// }
 }
